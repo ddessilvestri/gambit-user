9cfbb0812624c32d873fc66ded94eccd99d660ac
daviddexteel <dsilvestri@dexteel.com>
2025-05-22 19:48:53 -0300

    New commit

--- upload.ps1
+++ upload.ps1
@@ -2,9 +2,9 @@ git add .
 git commit -m "New commit"
 git push
 
-Set-Variable GOOS=linux
-Set-Variable GOARCH=amd64
-
+$env:GOOS = "linux"
+$env:GOARCH = "amd64"
 go build -o bootstrap main.go
+
 Remove-Item function.zip
 Compress-Archive -Path .\bootstrap -DestinationPath function.zip
