da6217e2c70459dd2ef635f6f0bb92932c75c559
daviddexteel <dsilvestri@dexteel.com>
2025-05-22 20:12:50 -0300

    New commit

--- upload.ps1
+++ upload.ps1
@@ -2,17 +2,9 @@ git add .
 git commit -m "New commit"
 git push
 
-# Configurar variables de entorno para compilar para Linux + x86_64
-$env:GOOS = "linux"
-$env:GOARCH = "amd64"
+Set-Variable GOOS=linux
+Set-Variable GOARCH=amd64
 
-# Compilar el ejecutable Go con nombre 'bootstrap' (requerido por runtime provided.al2)
 go build -o bootstrap main.go
-
-# Eliminar archivo ZIP previo si existe
-if (Test-Path "function.zip") {
-    Remove-Item function.zip
-}
-
-# Crear un archivo ZIP con el ejecutable bootstrap
+Remove-Item function.zip
 Compress-Archive -Path .\bootstrap -DestinationPath function.zip
